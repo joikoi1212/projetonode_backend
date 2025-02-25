ac5ced99a30ef6ff157d83e8570579ca21cfc53e
joikoi1212 <a.morais0101@gmail.com>
2025-02-25 15:39:01 +0000

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,7 +58,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Read THE DOCS
           </a>
         </div>
       </main>
